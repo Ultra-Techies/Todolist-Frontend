aebdd0bbfe21a2c29b1f7e15a67c0cd607a88db0
Jean Jepleting <44524100+jeanyego@users.noreply.github.com>
2022-03-04 09:21:18 +0300

    Feature/signup (#4)
    * Create db.json
    
    * update
    
    * Populated more Users and Performed Form Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Component } from '@angular/core';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent
     LoginComponent
   ],
   imports: [
-    BrowserModule
+   BrowserModule,
+   ReactiveFormsModule,
+   AppRoutingModule,
+   HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
