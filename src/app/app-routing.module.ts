8836dfe3a2cf7ea9ab604317789d9b5f5c455083
Martin Oywa <bonface.oywa@kyosk.app>
2022-03-04 09:32:38 +0300

    Add router links between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { Component } from '@angular/core';
 import { SignupComponent } from './signup/signup.component';
+import {LoginComponent} from "./login/login.component";
 // import { TodoComponent } from './todo/todo.component';
 const routes: Routes = [
   {path:'', redirectTo:'signup', pathMatch:'full'},
   // { path: 'todo', component:TodoComponent},
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
