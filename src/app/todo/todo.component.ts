3edda12138aed6cdbfbc769d2b2fe39d8407f09d
malcolmmaima <malcolmmaima@gmail.com>
2022-03-16 17:50:43 +0300

    task deleted successfully api call

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,34 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray,transferArrayItem  } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent implements OnInit{
-  url ='http://localhost:3000/created/'
-  constructor(private http: HttpClient, private router:Router) { }
-  created :any=[''];
-  posts:any=[];
-  done:any = [];
+export class TodoComponent implements OnInit {
+  url = 'http://localhost:3000/created/';
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+  created: any = [''];
+  posts: any = [];
+  done: any = [];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
-      this.updateTask(event.previousContainer.data[event.previousIndex], event.container.id);
+      this.updateTask(
+        event.previousContainer.data[event.previousIndex],
+        event.container.id
+      );
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
   }
   ngOnInit(): void {
-    console.log('ngOnInit')
+    console.log('ngOnInit');
 
     let header = new HttpHeaders();
     header.append('Content-Type', 'application/json');
@@ -38,66 +55,67 @@ export class TodoComponent implements OnInit{
     let userId = localStorage.getItem('userId');
 
     //call api to get all tasks
-    this.http.get('http://localhost:8080/api/task/'+userId, {headers: header})
-    .subscribe(res=>{
-      const createdTasks = Object.values(res);
-      console.log("Tasks: ",createdTasks);
+    this.http
+      .get('http://localhost:8080/api/task/' + userId, { headers: header })
+      .subscribe((res) => {
+        const createdTasks = Object.values(res);
+        console.log('Tasks: ', createdTasks);
 
-      //filter tasks
-      this.created = createdTasks.filter(task=>task.status === 'created');
-      this.posts = createdTasks.filter(task=>task.status === 'progress');
-      this.done = createdTasks.filter(task=>task.status === 'done');
+        //filter tasks
+        this.created = createdTasks.filter((task) => task.status === 'created');
+        this.posts = createdTasks.filter((task) => task.status === 'progress');
+        this.done = createdTasks.filter((task) => task.status === 'done');
+      });
+  }
+
+  showToastMessage(message: string = 'Your task is due') {
+    this.toastr.success(message);
+  }
+  deleteItem(id: any) {
+    this.http.delete('http://localhost:8080/api/task/delete/' + id).subscribe(
+      (res) => {
+        this.showToastMessage('Task Deleted Successfully');
+        this.ngOnInit();
+        this.router.navigate(['todo']);
+      },
+      (err: any) => {
+        console.log('Error: ', err);
+        this.showToastMessage('Unable to delete task, try again!');
+      }
+    );
+  }
+
+  //update task status on move to new section
+  updateTask(task: any, sectionId: String) {
+    console.log('New Status: ', sectionId);
+
+    //if section id is cdk-drop-list-0 then set task status to created,
+    //if section id is cdk-drop-list-1 then set task status to progress,
+    //if section id is cdk-drop-list-2 then set task status to done
+
+    if (sectionId === 'cdk-drop-list-0') {
+      task.status = 'created';
+    } else if (sectionId === 'cdk-drop-list-1') {
+      task.status = 'progress';
+    } else if (sectionId === 'cdk-drop-list-2') {
+      task.status = 'done';
     }
 
-    )
+    let header = new HttpHeaders();
+    header.append('Content-Type', 'application/json');
+    header.append('Access-Control-Allow-Origin', '*');
+    this.http
+      .put(`http://localhost:8080/api/task/update/${task.id}`, task, {
+        headers: header,
+      })
+      .subscribe((res) => {
+        console.log('Updated Task: ', res);
+        this.showToastMessage('Task updated successfully');
+        this.ngOnInit();
+      }),
+      (err: any) => {
+        this.showToastMessage('Unable to update task, try again!');
+        console.log('Error: ', err);
+      };
   }
-deleteUser(id: any){
-  // this.created.deleteUser(id).subscribe((result: any)=>{
-  //   console.log(result);
-  //   this.ngOnInit();
-  // return this.http.delete(`${this.url}/${id}`);
- 
-  // });
-  this.http.delete(`${this.url}/${id}`)
-  .subscribe(res=>{
- alert("Task Deleted Successfully");
- this.ngOnInit();
- this.router.navigate(['todo']);
-  },(err:any)=>{
-    alert("Unable to delete task")
-  })
-
-} 
-
-//update task status on move to new section
-updateTask(task:any, sectionId:String){
-  console.log("New Status: ",sectionId);
-
-  //if section id is cdk-drop-list-0 then set task status to created, 
-  //if section id is cdk-drop-list-1 then set task status to progress, 
-  //if section id is cdk-drop-list-2 then set task status to done 
-
-  if(sectionId === 'cdk-drop-list-0'){
-    task.status = 'created';
-  }
-  else if(sectionId === 'cdk-drop-list-1'){
-    task.status = 'progress';
-  }
-  else if(sectionId === 'cdk-drop-list-2'){
-    task.status = 'done';
-  }
-
-  let header = new HttpHeaders();
-  header.append('Content-Type', 'application/json');
-  header.append('Access-Control-Allow-Origin', '*');
-  this.http.put(`http://localhost:8080/api/task/update/${task.id}`,task,{headers:header})
-  .subscribe(res=>{
-    console.log("Updated Task: ",res);
-    this.ngOnInit();
-  }),(err:any)=>{
-    alert("Unable to update task, try again!")
-    console.log("Error: ",err);
-  }
-}
-
 }
