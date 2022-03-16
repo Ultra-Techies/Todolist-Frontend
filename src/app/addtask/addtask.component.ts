6a1fff6366cae32d4533a71efccdef6e79e4a0f4
malcolmmaima <malcolmmaima@gmail.com>
2022-03-16 17:57:03 +0300

    minor fixes, prettier auto-formated code

--- src/app/addtask/addtask.component.ts
+++ src/app/addtask/addtask.component.ts
@@ -7,51 +7,61 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-addtask',
   templateUrl: './addtask.component.html',
-  styleUrls: ['./addtask.component.css']
+  styleUrls: ['./addtask.component.css'],
 })
 export class AddtaskComponent implements OnInit {
-public addtaskForm !:FormGroup;
-  constructor( private formBuilder : FormBuilder, private http: HttpClient, private router : Router, private toastr:ToastrService) { }
+  public addtaskForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
   ngOnInit(): void {
     this.addtaskForm = this.formBuilder.group({
-      task:['', Validators.required],
-      duedate:['', Validators.required]
-    })
-    
+      task: ['', Validators.required],
+      duedate: ['', Validators.required],
+    });
   }
-addTask() {
-  let header = new HttpHeaders();
-  header.append('Content-Type', 'application/json');
-  header.append('Access-Control-Allow-Origin', '*');
-  let userId = localStorage.getItem('userId');
+  addTask() {
+    let header = new HttpHeaders();
+    header.append('Content-Type', 'application/json');
+    header.append('Access-Control-Allow-Origin', '*');
+    let userId = localStorage.getItem('userId');
 
-  let today = new Date();
+    let today = new Date();
 
-  const newTaskData = {
-    title: this.addtaskForm.value.task,
-    description: this.addtaskForm.value.task,
-    reminder: this.formatDate(this.addtaskForm.value.duedate),
-    dueDate: this.formatDate(this.addtaskForm.value.duedate),
-    createdTime: this.formatDate(today)
-  };
+    const newTaskData = {
+      title: this.addtaskForm.value.task,
+      description: this.addtaskForm.value.task,
+      reminder: this.formatDate(this.addtaskForm.value.duedate),
+      dueDate: this.formatDate(this.addtaskForm.value.duedate),
+      createdTime: this.formatDate(today),
+    };
 
-  this.http.post('http://localhost:8080/api/task/add/'+userId, newTaskData, {headers: header})
-  .subscribe(res => {
-    this.showToastMessage("Task added successfully");
-    this.addtaskForm.reset();
-    window.location.reload();
-  }, err => {
-    console.log("Error: "+err);
-    this.showToastMessage("Unable to add task, try again!");
-  })
-}
-
-showToastMessage(message:string = "Your task is due"){
-    this.toastr.success(message)
+    this.http
+      .post('http://localhost:8080/api/task/add/' + userId, newTaskData, {
+        headers: header,
+      })
+      .subscribe(
+        (res) => {
+          this.showToastMessage('Task added successfully');
+          this.addtaskForm.reset();
+          window.location.reload();
+        },
+        (err) => {
+          console.log('Error: ' + err);
+          this.showToastMessage('Unable to add task, try again!');
+        }
+      );
   }
-  
+
+  showToastMessage(message: string = 'Your task is due') {
+    this.toastr.success(message);
+  }
+
   //a function that takes date/time and converts it to this format: 2022-08-18 00:44:21
-  formatDate(date){
+  formatDate(date) {
     let date_ = new Date(date);
     let dd = String(date_.getDate()).padStart(2, '0');
     let mm = String(date_.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -63,4 +73,3 @@ showToastMessage(message:string = "Your task is due"){
     return newDate;
   }
 }
-
